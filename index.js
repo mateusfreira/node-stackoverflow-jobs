7ffe1699d3e6c4796dd112aecc2c288d90a68aec
Mateus Freira <mateus.freira@gmail.com>
2016-04-30 16:12:50 -0300

    Fixting anti-patterns

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-var rp = require('request-promise');
-var _ = require('lodash');
-var q = require('q');
-var parseString = require('xml2js').parseString;
+const rp = require('request-promise'),
+  _ = require('lodash'),
+  Promise = require("bluebird"),
+  parseString = Promise.promisify(require('xml2js').parseString);
 
-var StackoverflowJobs = function(){
-  var itemToJob = function(item){
+var StackoverflowJobs = function () {
+  var itemToJob = function (item) {
     return {
-      id : item.guid[0]._,
-      link : item.link[0],
+      id: item.guid[0]._,
+      link: item.link[0],
       category: item.category,
       title: item.title[0],
       description: item.description[0],
       pubDate: (item.pubDate ? new Date(item.pubDate[0]) : new Date()),
-      location : item.location ? item.location[0]._ : ""
+      location: item.location ? item.location[0]._ : ""
     };
   };
-  var mapRssToJob = function(rss){
+  var mapRssToJob = function (rss) {
     var channel = rss.channel[0];
     return {
-            total : channel['os:totalResults'],
-            description : channel.description,
-            jobs : channel.item.map(itemToJob)
-      };
+      total: channel['os:totalResults'],
+      description: channel.description,
+      jobs: channel.item.map(itemToJob)
+    };
   };
-  var toQueryString = function(obj) {
-    return _.map(obj,function(v,k){
+  var toQueryString = function (obj) {
+    return _.map(obj, function (v, k) {
       return encodeURIComponent(k) + '=' + encodeURIComponent(v);
     }).join('&');
   };
-  this.search = function(filters){
-    if(!filters.searchTerm){
-        filters.searchTerm = "";
+
+  this.search = function (filters) {
+    if (!filters.searchTerm) {
+      filters.searchTerm = "";
     }
-    return rp('http://careers.stackoverflow.com/jobs/feed?'+toQueryString(filters)).then(function(result){
-      var defer = q.defer();
-      parseString(result, function(err, obj){
-          if(!err){
-            defer.resolve(mapRssToJob(obj.rss));
-          }else{
-              defer.reject(err);
-          }
+    return rp('http://careers.stackoverflow.com/jobs/feed?' + toQueryString(filters)).then(function (feedsString) {
+      return parseString(feedsString).then(function (feedsObj) {
+        return mapRssToJob(feedsObj.rss);
       });
-      return defer.promise
     });
   };
 
-  this.allowsRemote = function(term){
-    return this.search({ searchTerm: term, allowsremote: true });
+  this.allowsRemote = function (term) {
+    return this.search({
+      searchTerm: term,
+      allowsremote: true
+    });
   };
-  this.searchTerm = function(term){
-      return this.search({ searchTerm: term});
+  this.searchTerm = function (term) {
+    return this.search({
+      searchTerm: term
+    });
   };
 };
 
--- test/search.spec.js
+++ test/search.spec.js
@@ -1,15 +1,15 @@
-describe('StackoverflowJobs', function(){
+describe('StackoverflowJobs', function () {
   var StackoverflowJobs = require('../index.js');
   var assert = require('assert');
   it(' search should return an object', function (done) {
-      StackoverflowJobs.searchTerm("node").then(function(result){
+    StackoverflowJobs.searchTerm("node").then(function (result) {
       assert.equal('object', typeof result);
       done();
     });
   });
 
   it('search should return the values total, description, jobs ', function (done) {
-      StackoverflowJobs.searchTerm("node").then(function(result){
+    StackoverflowJobs.searchTerm("node").then(function (result) {
       assert.notEqual(undefined, result.total);
       assert.notEqual(undefined, result.description);
       assert.notEqual(undefined, result.jobs);
@@ -17,8 +17,8 @@ describe('StackoverflowJobs', function(){
     });
   });
   it('search should allow empty search parameter ', function (done) {
-       this.timeout(50000);
-      StackoverflowJobs.searchTerm().then(function(result){
+    this.timeout(50000);
+    StackoverflowJobs.searchTerm().then(function (result) {
       assert.notEqual(undefined, result.total);
       assert.notEqual(undefined, result.description);
       assert.notEqual(undefined, result.jobs);
@@ -27,20 +27,21 @@ describe('StackoverflowJobs', function(){
   });
 
 
-    it('jobs should return the fields link, category, title, description, pubDate, location on allowsremote', function (done) {
-        this.timeout(50000);
-        StackoverflowJobs.allowsRemote("node").then(function(result){
-        assert.notEqual(undefined, result.jobs[0].link);
-        assert.notEqual(undefined, result.jobs[0].category);
-        assert.notEqual(undefined, result.jobs[0].title);
-        assert.notEqual(undefined, result.jobs[0].description);
-        assert.notEqual(undefined, result.jobs[0].pubDate);
-        assert.notEqual(undefined, result.jobs[0].location);
-        done();
-      });
+  it('jobs should return the fields link, category, title, description, pubDate, location on allowsremote', function (done) {
+    this.timeout(50000);
+    StackoverflowJobs.allowsRemote("node").then(function (result) {
+      assert.notEqual(undefined, result.jobs[0].link);
+      assert.notEqual(undefined, result.jobs[0].category);
+      assert.notEqual(undefined, result.jobs[0].title);
+      assert.notEqual(undefined, result.jobs[0].description);
+      assert.notEqual(undefined, result.jobs[0].pubDate);
+      assert.notEqual(undefined, result.jobs[0].location);
+      done();
     });
+  });
+
   it('jobs should return the fields link, category, title, description, pubDate, location', function (done) {
-      StackoverflowJobs.searchTerm("node").then(function(result){
+    StackoverflowJobs.searchTerm("node").then(function (result) {
       assert.notEqual(undefined, result.jobs[0].link);
       assert.notEqual(undefined, result.jobs[0].category);
       assert.notEqual(undefined, result.jobs[0].title);
